d2526c95de96029580f8a361a2f4de2c9e92f896
suravimondal <73650143+suravimondal@users.noreply.github.com>
2021-03-26 17:22:22 +0530

    Add files via upload

---
+++ Box.js
@@ -0,0 +1,31 @@
+class Box{
+    constructor(x,y,width,height){
+        var options={
+            restitution: 0.9
+        }
+            // this is used for referencing to the object
+        this.body= Bodies.rectangle(x,y,width,height,options);
+        this.width= width;
+        this.height= height;
+        World.add(world,this.body);
+    }
+
+    display(){
+                //renaming or namespacing 
+            var pos= this.body.position;
+            var angle= this.body.angle;
+
+            push();
+            translate(pos.x, pos.y);
+
+            rotate(angle);
+
+            rectMode(CENTER);
+            fill(255);
+            rect(0,0,this.width,this.height);
+            pop();
+
+    }
+
+
+}
